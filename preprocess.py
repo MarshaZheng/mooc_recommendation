5064f1beb4f9bacc35e398ecf596e4b8936acc48
MarshaZheng <2001210557@stu.pku.edu.cn>
2021-03-30 09:06:33 +0800

    init

--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,13 @@
-# -*- coding: utf-8 -*-
-# Origin resource from MovieLens: http://grouplens.org/datasets/movielens/1m
+# -*- coding:utf-8 -*-
+import csv
+import json
+import sys
+import codecs
 import pandas as pd
-
 
 class Channel:
     """
-    simple processing for *.dat to *.csv
+    将json转换成csv
     """
 
     def __init__(self):
@@ -14,26 +16,17 @@ class Channel:
     def process(self):
         print('Process user data...')
         self._process_user_data()
-        print('Process movies data...')
+        print('Process courses data...')
         self._process_movies_date()
-        print('Process rating data...')
-        self._process_rating_data()
+        print('Process actions data...')
+        self._process_actions_data()
         print('End.')
 
-    def _process_user_data(self, file='users.dat'):
-        f = pd.read_table(self.origin_path.format(file), sep='::', engine='python',
-                          names=['userID', 'Gender', 'Age', 'Occupation', 'Zip-code'])
-        f.to_csv(self.origin_path.format('users.csv'), index=False)
+    def _process_user_data(self, file=''):
 
-    def _process_rating_data(self, file='ratings.dat'):
-        f = pd.read_table(self.origin_path.format(file), sep='::', engine='python',
-                          names=['UserID', 'MovieID', 'Rating', 'Timestamp'])
-        f.to_csv(self.origin_path.format('ratings.csv'), index=False)
+    def _process_actions_data(self, file=''):
 
-    def _process_movies_date(self, file='movies.dat'):
-        f = pd.read_table(self.origin_path.format(file), sep='::', engine='python',
-                          names=['MovieID', 'Title', 'Genres'])
-        f.to_csv(self.origin_path.format('movies.csv'), index=False)
+    def _process_courses_date(self, file=''):
 
 
 if __name__ == '__main__':
